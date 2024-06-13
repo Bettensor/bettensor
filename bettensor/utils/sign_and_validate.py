2615bf6d683b361f0e2e60ec491c1e506d77c77c
root <root@vmi1891726.contaboserver.net>
2024-06-13 17:42:07 +0200

    all changes to fix signing and DB insertion

--- bettensor/utils/sign_and_validate.py
+++ bettensor/utils/sign_and_validate.py
@@ -8,18 +8,28 @@ import bittensor as bt
 
 
 def create_signature(data: str, wallet: bt.wallet) -> str:
-    '''
+    """Signs the given data with the wallet hotkey
     
+    Arguments:
+        wallet:
+            The wallet used to sign the Data
+        data:
+            Data to be signed
     
-    '''
+    Returns:
+        signature:
+            Signature of the key signing for the data
+    """
     try:
-        signature = wallet.hotkey.sign(data.encode().hex())
+        signature = wallet.hotkey.sign(data.encode()).hex()
         return signature
-    except Exception as e:
-        print(f"Error signing data: {e}")
-        return None
-
-
+    except TypeError as e:
+        bt.logging.error(f'Unable to sign data: {data} with wallet hotkey: {wallet.hotkey} due to error: {e}')
+        raise TypeError from e
+    except AttributeError as e:
+        bt.logging.error(f'Unable to sign data: {data} with wallet hotkey: {wallet.hotkey} due to error: {e}')
+        raise AttributeError from e
+        
 def verify_signature(hotkey: str, data: str, signature: str) -> bool:
     
     try:
