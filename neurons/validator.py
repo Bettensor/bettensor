3fa097d0795c0a6f4277bd28047e578f3a8d810e
oneandahalfcats <sw@nickel5.com>
2024-07-27 06:07:33 +0300

    removing async

--- neurons/validator.py
+++ neurons/validator.py
@@ -214,7 +214,7 @@ async def main(validator: BettensorValidator):
             if current_block - validator.last_updated_block > 150:
                 # Sends data to the website
                 try:
-                    result = await fetch_and_send_predictions(db_path="data/validator.db")
+                    result = await validator.run_sync_in_async(lambda: fetch_and_send_predictions("data/validator.db"))
                     bt.logging.info(f"Result status: {result}")
                     if result:
                         bt.logging.info("Predictions fetched and sent successfully")
