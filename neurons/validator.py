06b099ab9f85edb9106836ee7616e0ef4f7b02ad
geardici <29338530+geardici@users.noreply.github.com>
2024-08-14 10:22:58 -0700

    soccer leagues updates.

--- neurons/validator.py
+++ neurons/validator.py
@@ -61,16 +61,56 @@ async def main(validator: BettensorValidator):
             {"id": "1", "season": "2024"}
         ],
         "soccer": [
-            {"id": "253", "season": "2024"},  # MLS
-            {"id": "140", "season": "2024"},  # La Liga
-            {"id": "78", "season": "2024"},   # Bundesliga
-            {"id": "262", "season": "2024"},  # Liga MX
-            {"id": "4", "season": "2024"},     # Euro Cup
-            {"id": "9", "season": "2024"},    # Copa America
-            {"id": "71", "season": "2024"},  # Brasileirão Série A
-            {"id": "98", "season": "2024"}, # J1 League
-            {"id": "480", "season": "2024"}, # Olympics mens
-            {"id": "524", "season": "2024"} # Olympics womens
+            {"id": "2", "season": 2024},  # UEFA Champions League, World
+            {"id": "3", "season": 2024},  # UEFA Europa League, World
+            {"id": "848", "season": 2024},  # UEFA Europa Conference League, World
+            {"id": "531", "season": 2024},  # UEFA Super Cup, World
+            {"id": "140", "season": 2024},  # La Liga, Spain
+            {"id": "78", "season": 2024},  # Bundesliga, Germany
+            {"id": "135", "season": 2024},  # Serie A, Italy
+            {"id": "39", "season": 2024},  # Premier League, England
+            {"id": "61", "season": 2024},  # Ligue 1, France
+            {"id": "94", "season": 2024},  # Primeira Liga, Portugal
+            {"id": "71", "season": 2024},  # Serie A, Brazil
+            {"id": "97", "season": 2024},  # Taça da Liga, Portugal
+            {"id": "73", "season": 2024},  # Copa Do Brasil, Brazil
+            {"id": "253", "season": 2024},  # Major League Soccer, USA
+            {"id": "307", "season": 2024},  # Pro League, Saudi Arabia
+            {"id": "262", "season": 2024},  # Liga MX, Mexico
+            {"id": "12", "season": 2024},  # CAF Champions League, World
+            {"id": "13", "season": 2024},  # CONMEBOL Libertadores, World
+            {"id": "11", "season": 2024},  # CONMEBOL Sudamericana, World
+            {"id": "81", "season": 2024},  # DFB Pokal, Germany
+            {"id": "137", "season": 2024},  # Coppa Italia, Italy
+            {"id": "526", "season": 2024},  # Trophée des Champions, France
+            {"id": "529", "season": 2024},  # Super Cup, Germany
+            {"id": "556", "season": 2024},  # Super Cup, Spain
+            {"id": "103", "season": 2024},  # Eliteserien, Norway
+            {"id": "98", "season": 2024},  # J1 League, Japan
+            {"id": "274", "season": 2024},  # Liga 1, Indonesia
+            {"id": "218", "season": 2024},  # Bundesliga, Austria
+            {"id": "219", "season": 2024},  # 2. Liga, Austria
+            {"id": "104", "season": 2024},  # 1. Division, Norway
+            {"id": "107", "season": 2024},  # I Liga, Poland
+            {"id": "88", "season": 2024},  # Eredivisie, Netherlands
+            {"id": "540", "season": 2024},  # CONMEBOL Libertadores U20, World
+            {"id": "383", "season": 2024},  # Ligat Ha'al, Israel
+            {"id": "303", "season": 2024},  # Division 1, United-Arab-Emirates
+            {"id": "304", "season": 2024},  # Liga Panameña de Fútbol, Panama
+            {"id": "611", "season": 2024},  # Brasiliense, Brazil
+            {"id": "77", "season": 2024},  # Alagoano, Brazil
+            {"id": "602", "season": 2024},  # Baiano - 1, Brazil
+            {"id": "624", "season": 2024},  # Carioca - 1, Brazil
+            {"id": "604", "season": 2024},  # Catarinense - 1, Brazil
+            {"id": "609", "season": 2024},  # Cearense - 1, Brazil
+            {"id": "475", "season": 2024},  # Paulista - A1, Brazil
+            {"id": "622", "season": 2024},  # Pernambucano - 1, Brazil
+            {"id": "306", "season": 2024},  # Qatar Stars League, Qatar
+            {"id": "19", "season": 2024},  # AFC Champions League, Asia
+            {"id": "101", "season": 2024},  # J-League Cup, Japan
+            {"id": "72", "season": 2024},  # Serie B, Brazil
+            {"id": "136", "season": 2024},  # Serie B, Italy
+            {"id": "812", "season": 2024},  # Super Cup, Belarus
         ]
     }
 
