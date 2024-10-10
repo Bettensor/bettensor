a9493643023446811e9c6ffcec6a23116cf435a6
root <root@vmi2187488.contaboserver.net>
2024-10-10 14:48:37 -0700

    miner.py merge

--- neurons/miner.py
+++ neurons/miner.py
@@ -176,12 +176,6 @@ def main(miner: BettensorMiner):
                         < miner.predictions_handler.models[
                             "soccer"
                         ].minimum_wager_amount
-<<<<<<< Updated upstream
-                        and not miner.predictions_handler.models[
-                            "soccer"
-                        ].made_daily_predictions
-=======
->>>>>>> Stashed changes
                     ):
                         bt.logging.warning(
                             f"Miner cash is insufficient for soccer model predictions. Skipping this step."
